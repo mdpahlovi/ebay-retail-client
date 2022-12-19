c429a7812e8c7535c864047ec13c85107a6bbad8
MD Pahlovi <mdpahlovi07@gmaill.com>
2022-12-19 22:54:24 +0600

    App Added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,6 +5,9 @@ import { Link } from "react-router-dom";
 import Advertisement from "./Components/Advertisement";
 import Hero from "./Components/Hero";
 import ServiceCard from "../../Components/ServiceCard";
+import Google_Badge from "../../Assets/logo/Google_Play_Store_badge_EN.png";
+import Apple_Badge from "../../Assets/logo/Download_on_the_App_Store_Badge.png";
+import App_img from "../../Assets/mobility_illustration.webp";
 
 const Home = () => {
     const { isLoading, data } = useQuery({
@@ -41,9 +44,7 @@ const Home = () => {
                         <button type="submit" className="btn btn-primary">
                             Contact Us
                         </button>
-                        <Link aria-label="" className="btn btn-outline btn-primary">
-                            Learn more
-                        </Link>
+                        <Link className="btn btn-outline btn-primary">Learn more</Link>
                     </div>
                 </div>
                 <div className="grid gap-5 sm:grid-cols-2">
@@ -52,6 +53,21 @@ const Home = () => {
                     <ServiceCard stats="60" statsTitle="Total Seller" />
                     <ServiceCard stats="20" statsTitle="Daily Customer" />
                 </div>
+            </section>
+            <section className="my-container section-gap bg-primary/10 px-10 py-8 grid grid-cols-1 md:grid-cols-2 items-center gap-8 rounded-lg">
+                <div class="space-y-6">
+                    <h1>Download it now</h1>
+                    <p class="text-lg">Be part of millions people around the world using tailus in modern User Interfaces.</p>
+                    <div class="flex flex-wrap justify-center gap-4 md:justify-start lg:gap-6">
+                        <a href="/" target="blank">
+                            <img src={Google_Badge} className="w-36" alt="Google Badge" />
+                        </a>
+                        <a href="/" target="blank">
+                            <img src={Apple_Badge} className="w-36" alt="Apple store Badge" />
+                        </a>
+                    </div>
+                </div>
+                <img src={App_img} class="m-auto w-80" loading="lazy" alt="mobility_illustration" />
             </section>
         </>
     );
